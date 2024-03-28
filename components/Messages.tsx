450d143cbf601f4238a825f54c82966328145799
Rohan Adnan <bc230430189ruh@vu.edu.pk>
2024-03-29 02:15:01 +0500

    final design done ready to deploy

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,29 +1,27 @@
-'use client'
+"use client";
 
-import { ChatMessage } from "@/app/page"
-import { ChevronDownCircle } from "lucide-react"
-import Loader from "./Loader"
-import { useSelector } from "react-redux"
-import { useFormStatus } from "react-dom"
+import { ChatMessage } from "@/app/page";
+import { ChevronDownCircle } from "lucide-react";
+import Loader from "./Loader";
+import { useSelector } from "react-redux";
+import { useFormStatus } from "react-dom";
 
 interface Props {
-    messages: ChatMessage[]
+  messages: ChatMessage[];
 }
 
-const Messages = ({messages}: Props) => {
+const Messages = ({ messages }: Props) => {
   const { pending } = useFormStatus();
-  const {displaySettings, recording} = useSelector((state: any) => state.app)
+  const { displaySettings, recording } = useSelector((state: any) => state.app);
 
-    return(
-        <div
+  return (
+    <div
       className={`flex flex-col min-h-screen p-5 pt-20 ${
         messages.length > 0 ? "pb-96" : "pb-32"
       }`}
     >
-
-      <Loader/>
+      <Loader />
       <div className="flex flex-col flex-1">
-
         {!messages.length && !displaySettings && !pending && !recording && (
           <div className="flex flex-col space-y-6 flex-1 items-center justify-end">
             <p className="animate-pulse text-black">Start a conversation</p>
@@ -34,28 +32,30 @@ const Messages = ({messages}: Props) => {
           </div>
         )}
 
-        <div className="p-5 space-y-5">
-          {messages.map((message) => (
-            <div key={message.id} className="space-y-5">
-              {/* reciever */}
-              <div className="pr-48">
-                <p className="message bg-gray-800 rounded-bl-none">
-                  {message.response}
-                </p>
-              </div>
+        <div className="max-w-3xl m-auto">
+          <div className="p-5 space-y-5">
+            {messages.map((message) => (
+              <div key={message.id} className="space-y-5">
+                {/* reciever */}
+                <div className="pr-48">
+                  <p className="message bg-gray-800 rounded-bl-none">
+                    {message.response}
+                  </p>
+                </div>
 
-              {/* sender */}
-              <div className="pl-48">
-                <p className="message text-left ml-auto rounded-br-none bg-gray-600">
-                  {message.sender}
-                </p>
+                {/* sender */}
+                <div className="pl-48">
+                  <p className="message text-left ml-auto rounded-br-none bg-gray-600">
+                    {message.sender}
+                  </p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default Messages
+export default Messages;
